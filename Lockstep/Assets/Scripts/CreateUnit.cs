bd04afd75ab80c7f7f57461f5c6b6bab223c3ae3
Saika-m <165717332+saika-m@users.noreply.github.com>
2024-04-24 01:19:15 -0400

    initial commit

---
+++ Lockstep/Assets/Scripts/CreateUnit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using System.Text;
+
+[Serializable]
+public class CreateUnit : Action
+{
+    public string Uuid;
+    public int Xpos;
+    public int Zpos;
+    public int Speed;
+    public int Direction;
+    
+    public CreateUnit(int owningPlayer, int lockstepTurnID, string actionName, string uuid, int xpos, int zpos, int speed, int direction) : base(owningPlayer, lockstepTurnID, actionName)
+    {
+        Uuid = uuid;
+        Xpos = xpos;
+        Zpos = zpos;
+        Speed = speed;
+        Direction = direction;
+        
+        //Debug.LogError("CreateUnit frame: " + gameFrame);
+        //Debug.LogError("unitData Count: " + unitData.Length);
+
+
+        //Debug.LogError("CreatUnit Action with Hash " + Hash);
+    }
+
+    public override void ProcessAction()
+    {
+        //if (ObjectPool.Instance.GetGameState().Length > 25) return;
+
+        GameObject b = ObjectPool.Instance.GetBuilding(OwningPlayer);
+
+        var move = b.GetComponent<Unit>();
+        var data = move.UnitData;
+
+        data.Uuid = Uuid;
+
+        data.Direction = Direction;
+        data.Speed = Speed;
+
+        move.UnitData = data;
+        
+        //move.UnitData.Direction = Direction;
+        //move.UnitData.Speed = Speed;
+
+        move.SetUp();
+
+        b.transform.position = new Vector3(Xpos, 0f, Zpos);
+    }
+}
---
+++ Lockstep/Assets/Scripts/NetworkHandler.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkHandler : MonoBehaviourPunCallbacks
+{
+    public System.Action JoinedRoom;
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN.");
+
+        RoomOptions roomOptions = new RoomOptions
+        {
+            IsVisible = false,
+            MaxPlayers = 4
+        };
+
+        PhotonNetwork.ConnectToRegion("eu");
+        PhotonNetwork.JoinOrCreateRoom("The room", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("Joined room");
+        JoinedRoom?.Invoke();
+    }
+}
